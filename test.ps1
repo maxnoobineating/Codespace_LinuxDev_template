f4549c7461d913ad95fc82985f198154f02831ce
maxnoobineating <zsdezsc@gmail.com>
2024-05-01 22:34:52 +0800

    5/1/2024

--- test.ps1
+++ test.ps1
@@ -1,19 +1,19 @@
-$prevWindowTitle = ""
+$ClipPath_host=$(Join-Path $PWD.PATH "clipboard.txt")
+$ClipExePath="C:\Windows\System32\clip.exe"
 
+# while ($true) {
+#     # Get-Content $ClipPath_host
+#     Get-Content $ClipPath_host | & $ClipExePath
+#     # Wait a short interval before checking again (adjust as needed)
+#     Start-Sleep -Seconds 1
+# }
+
+$lastWriteFile = Get-Item $ClipPath_host
 while ($true) {
-  # Get the foreground process and window title
-  $foregroundProcess = Get-Process | Where-Object {$_.MainWindowTitle }
-  $windowTitle = $foregroundProcess.MainWindowTitle
-
-  # Check if window title changed (assuming focus change)
-  if ($windowTitle -ne $prevWindowTitle) {
-    Write-Host "Terminal window $windowTitle lost focus!"
-    # You can add your desired actions here (e.g., clipboard sync)
-  }
-
-  # Update previous window title for comparison
-  $prevWindowTitle = $windowTitle
-
-  # Wait a short interval before checking again (adjust as needed)
-  Start-Sleep -Seconds 1
+    Start-Sleep -Seconds 1
+    $currentWriteFile = Get-Item $ClipPath_host
+    if ($currentWriteFile.LastWriteTime.ticks -ne $lastWriteFile.LastWriteTime.ticks) {
+        Get-Content $currentWriteFile 
+        $lastWriteFile = $currentWriteFile
+    }
 }
